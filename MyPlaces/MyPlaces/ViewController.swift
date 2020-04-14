5acf12c359f001f7c4b2cf5dbe4d2932a70adc4a
Natallia <tarasevich.natallia@gmail.com>
2020-04-14 17:18:21 +0300

    Add TableView, constraints, TableViewCell

---
+++ MyPlaces/MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Natallia Tarasevich on 4/14/20.
+//  Copyright © 2020 Natallia Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
