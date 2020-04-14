2705f36fee0b4c3aeee54fd0ff0c629dfc1ce973
Natallia <tarasevich.natallia@gmail.com>
2020-04-14 17:51:05 +0300

    Add 2 fuknc for TableView, Add NavigationController, add let cell

--- MyPlaces/MyPlaces/ViewController.swift
+++ MyPlaces/MyPlaces/ViewController.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 10
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+       }
 
 
 }
